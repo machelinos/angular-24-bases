a288b64510c29ef6a3eb0ac38f61221d38f2f8e9
Marcel Cabrera <marcel.cabrera@gmail.com>
2024-10-07 16:46:10 -0600

    counter functionality inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'My first app';
   public counter: number = 10;
+
+  onIncreaseClick(value: number) {
+    this.counter += value;
+  } 
+
+  onResetClick(): void {
+    this.counter = 10;
+  }
+
+
 }
