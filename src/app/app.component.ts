5063745b7688736c3e351ab8fc471c203ed80c0e
Marcel Cabrera <marcel.cabrera@gmail.com>
2024-10-07 17:18:10 -0600

    Counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'My first app';
-  public counter: number = 10;
-
-  onIncreaseClick(value: number) {
-    this.counter += value;
-  } 
-
-  onResetClick(): void {
-    this.counter = 10;
-  }
-
-
 }
